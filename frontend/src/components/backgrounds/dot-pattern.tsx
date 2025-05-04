4846fd4292753b538b86d1b2e4b293d07872288d
iemio <deeplearner4u@gmail.com>
2025-05-05 01:38:00 +0530

    fix

--- frontend/src/components/backgrounds/dot-pattern.tsx
+++ frontend/src/components/backgrounds/dot-pattern.tsx
@@ -134,7 +134,7 @@ export function DotPattern({
                     />
                 </radialGradient>
             </defs>
-            {dots.map((dot, _) => (
+            {dots.map((dot) => (
                 <motion.circle
                     key={`${dot.x}-${dot.y}`}
                     cx={dot.x}
