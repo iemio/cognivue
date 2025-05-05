b7bf7a16e118cc419f1ad517c33faa83a3d259d3
iemio <deeplearner4u@gmail.com>
2025-05-06 00:56:40 +0530

    fix

--- frontend/src/components/personal/achievement/3d-card.tsx
+++ frontend/src/components/personal/achievement/3d-card.tsx
@@ -34,12 +34,12 @@ export const CardContainer = ({
         const y = (e.clientY - top - height / 2) / 25;
         containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
     };
-
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
         setIsMouseEntered(true);
         if (!containerRef.current) return;
     };
-
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
         if (!containerRef.current) return;
         setIsMouseEntered(false);
@@ -116,6 +116,7 @@ export const CardItem = ({
     rotateX?: number | string;
     rotateY?: number | string;
     rotateZ?: number | string;
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any;
 }) => {
     const ref = useRef<HTMLDivElement>(null);
