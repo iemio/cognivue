037293e6a18b340ce1c08362f6789d661dac5276
iemio <deeplearner4u@gmail.com>
2025-05-25 18:21:15 +0530

    minor fix

--- frontend/src/features/electric-workflow/hooks/useUpdateData.tsx
+++ frontend/src/features/electric-workflow/hooks/useUpdateData.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { Edge, Node, ReactFlowJsonObject } from "@xyflow/react";
 // import axios from "axios";
 
